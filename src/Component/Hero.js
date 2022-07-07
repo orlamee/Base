136b2f7b431469031b4110d3311643cc6c805e19
Olamilekan Akindele <olalekan@astract.com.ng>
2022-07-07 12:44:21 +0100

    Hero

--- src/Component/Hero.js
+++ src/Component/Hero.js
@@ -5,7 +5,9 @@ import wework from "../img/wework.png";
 import valley from "../img/valley.png";
 import hub from "../img/hub.png";
 import work from "../img/workstation.png";
+// import pin from "../img/pin.png";
 import seedspace from "../img/seedspace.png";
+// import soon from "../img/coming-soon.png";
 // import Animation from "./Animation";
 
 function Hero() {
@@ -19,7 +21,8 @@ function Hero() {
             <h2 className='hero'>best <span style={{color: "#00ABE7"}}>workspace</span></h2>
             {/* <Animation/> */}
             <h2 className='hero'>in your city</h2>
-            <h6 className="text-muted pt-4 fs-5">Ditch inconsistent power supply at home, check-in to a workspace near you, pay per minute</h6>
+            <h6 className="text-muted pt-4 fs-5">Ditch inconsistent power supply at home, check-in to a</h6>
+            <h6 className="text-muted fs-5">workspace near you, pay per minute</h6>
             <div className="d-grid gap-2 d-md-block mt-5">
               <button className="btn btn-primary me-3 py-3 px-3" type="button">Sign up for free</button>
               <button className="btn btn-outline-dark py-3 px-3 text-secondary" type="button">Teams - Get started today</button>
@@ -37,13 +40,31 @@ function Hero() {
           </div>
         </div>
         <div className="row justify-content-center">
-          <div className="col-md-10 logo-sect text-center">
-            <img className="d-inline me-3" alt="" src={wework}/>
-            <img className="d-inline me-3" alt="" src={valley}/>
-            <img className="d-inline me-3" alt="" src={seedspace}/>
-            <img className="d-inline me-3" alt="" src={work}/>
-            <img className="d-inline me-3" alt="" src={hub}/>
-            {/* <div className="d-inline p-2 bg-dark text-white">d-inline</div> */}
+          <div className="col-md-11 logo-sect text-center">
+            <img className="d-inline me-3 img-fluid" alt="" src={wework}/>
+            <img className="d-inline me-3 img-fluid" alt="" src={valley}/>
+            <img className="d-inline me-3 img-fluid" alt="" src={seedspace}/>
+            <img className="d-inline me-3 img-fluid" alt="" src={work}/>
+            <img className="d-inline me-3 img-fluid" alt="" src={hub}/>
+          </div>
+        </div>
+        <div className="row justify-content-center mb-5">
+          <div className="col-md-5 mb-3">
+            <div className="d-flex flex-row pin">
+              <i className="fa fa-map-pin icon-hero me-3" aria-hidden="true"></i>
+              <h6>Live in: Lagos, Port Harcourt, Abeokuta, Abuja</h6>
+            </div>
+          </div>
+          <div className="col-md-5">
+            <div className="d-flex flex-row pin">
+              <i className="fa fa-bullhorn icon-hero me-3" aria-hidden="true"></i>
+              <h6>Coming soon: Ibadan, Owerri, Accra, Nairobi</h6>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center hero-button">
+          <div className="col-md-5 text-center">
+            <button className="btn btn-primary me-3 py-3 px-5 shadow" type="button">Try for free</button>
           </div>
         </div>
       </div>

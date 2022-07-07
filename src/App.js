6147f12fe5ddde7f50f26a2490e096248d151f20
Olamilekan Akindele <olalekan@astract.com.ng>
2022-07-07 17:36:33 +0100

    SecondLayer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './Component/Header';
 import React from 'react';
 import Gradient from './Component/Gradient';
+import Secondlayer from './Component/SecondLayer';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Gradient/>
+      <Secondlayer/>
     </div>
   );
 }
---
+++ src/Component/SecondLayer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import venia from "../img/venia.svg";
+function Secondlayer() {
+  return (
+    <section className="secondlayer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <img src={venia} alt="" className="img-fluid" />
+          </div>
+          <div className="col-md-6">
+            <h2 className="hero"><span style={{color: "#00ABE7"}}>No advance booking,</span><br/>just walk in</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Secondlayer;

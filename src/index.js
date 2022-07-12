15b8450edb2e8103118d6bb2fb77acd792565ac9
Olamilekan Akindele <olalekan@astract.com.ng>
2022-07-12 01:50:20 +0100

    web

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import Login from './Login';
 import Loginas from './Login-as';
-import reportWebVitals from './reportWebVitals';
 import CreateAccount from './CreateAccount';
 import {
   BrowserRouter,
@@ -33,4 +32,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();

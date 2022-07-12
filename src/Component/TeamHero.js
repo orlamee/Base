e9d3f817797de44bc48a03cbe28d325f0bcd2db4
Olamilekan Akindele <olalekan@astract.com.ng>
2022-07-12 01:42:17 +0100

    Team

--- src/Component/TeamHero.js
+++ src/Component/TeamHero.js
@@ -1,20 +1,22 @@
 import React from "react";
 import heroteam from "../img/teamhero.png";
+import mobile from "../img/teamhero-mobile.png";
 
 function TeamHero() {
   return(
     <section>
       <div className="" style={{marginTop: "150px"}}>
-        <img src={heroteam} alt="" className="bg img-fluid" />
+        <img src={heroteam} alt="" className="bg img-fluid desktop" />
+        <img src={mobile} alt="" className="bg img-fluid mobile" />
       </div>
-      <div className="banner-inner">
+      <div className="banner-inn">
 				<div className="row subtext justify-content-center" data-aos="slide-down" data-aos-duration="2000">
           <div className="col-md-8 text-center">
             <h2 className='heroteam'>Increase your teams’ productivity</h2>
             {/* <Animation/> */}
             <div className="d-grid gap-2 d-md-block mt-5">
-              <button className="btn btn-primary me-3 py-3 px-3" type="button">Try it now</button>
-              <button className="btn btn-outline-dark py-3 px-3 text-secondary" type="button">Send me a brochure</button>
+              <button className="btn btn-primary me-3 py-2 px-5" type="button">Try it now</button>
+              <button className="btn btn-outline-dark py-2 px-3 text-secondary" type="button">Send me a brochure</button>
             </div>
           </div>
 				</div>

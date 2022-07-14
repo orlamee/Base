cca12aacc6b238bf282c4dffa09efffb564fbec4
Olamilekan Akindele <olalekan@astract.com.ng>
2022-07-14 21:38:04 +0100

    Explore

---
+++ src/Component/Location.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Location(params) {
+  return (
+    <section className="location-section">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-11 lagos text-center">
+            <h2>Lagos</h2>
+          </div>
+          <div className="col-md-10 abuja text-center">
+            <h2>Abuja</h2>
+          </div>
+          <div className="col-md-9 port text-center">
+            <h2>Port Harcourt</h2>
+          </div>
+          <div className="col-md-8 abeokuta text-center">
+            <h2>Abeokuta</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Location;
--- src/ExploreSpace.js
+++ src/ExploreSpace.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ExploreHero from "./Component/ExploreHero";
+import Footer from "./Component/Footer";
 import Header from "./Component/Header";
+import Location from "./Component/Location";
+import SeventhLayer from "./Component/SeventhLayer";
 
 function ExploreSpace() {
   return (
     <div>
       <Header/>
       <ExploreHero/>
+      <Location/>
+      <SeventhLayer/>
+      <Footer/>
     </div>
   )
 }

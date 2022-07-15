e80dd12f709922d74b6ecfe82eb798b1a69f30ec
Olamilekan Akindele <olalekan@astract.com.ng>
2022-07-15 10:25:04 +0100

    Explore

--- src/Component/ExploreHero.js
+++ src/Component/ExploreHero.js
@@ -11,13 +11,8 @@ function ExploreHero() {
       </div>
       <div className="banner-inn">
 				<div className="row subtext justify-content-center" data-aos="slide-down" data-aos-duration="2000">
-          <div className="col-md-8 text-center">
+          <div className="col-md-8 text-center" style={{marginTop: "30px"}}>
             <h2 className='heroteam'>Explore Spaces</h2>
-            {/* <Animation/> */}
-            {/* <div className="d-grid gap-2 d-md-block mt-5">
-              <button className="btn btn-primary me-3 py-2 px-5" type="button">Try it now</button>
-              <button className="btn btn-outline-dark py-2 px-3 text-secondary" type="button">Send me a brochure</button>
-            </div> */}
           </div>
 				</div>
 			</div>

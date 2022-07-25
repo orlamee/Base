7c73e470a2b0b98f587384f6ec7994dcd35f1a4f
Olamilekan Akindele <olalekan@astract.com.ng>
2022-07-25 19:23:39 +0100

    Waitlist

--- src/Component/WaitList.js
+++ src/Component/WaitList.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { useForm, ValidationError } from '@formspree/react';
+// import yes from "../img/yes.png";
 
 function WaitList() {
+  const [state, handleSubmit] = useForm("meqnynll");
+  if (state.succeeded) {
+    return <h6 className="mt-4" style={{color: "green"}}>
+      You have successfully joined our waitlist
+    </h6>;
+  }
   return (
     <div className="waitlist justify-content-center mt-5">
-      <form className="form-inline " method="post">
-        <div className="form-group"><input className="form-control" type="email" name="email" placeholder="Your Email"/></div>
-        <div className="form-group"><button className="btn btn-primary" type="button">Join the waitlist </button></div>
+      <form className="form-inline" onSubmit={handleSubmit}>
+        <div className="form-group"><input className="form-control" id="email" type="email" name="email" placeholder="Your Email"/></div>
+        <ValidationError 
+          prefix="Email" 
+          field="email"
+          errors={state.errors}
+        />
+        <div className="form-group"><button type="submit" disabled={state.submitting} className="btn btn-primary" id="my-form-button">Join the waitlist </button></div>
+        <p id="my-form-status"></p>
       </form>
     </div>
-  )
+  );
 }
 
 export default WaitList;

5e9ed8c3b0122f74add21b6df6b4220bd1214935
Olamilekan Akindele <olalekan@astract.com.ng>
2022-07-26 20:44:44 +0100

    Waitlist

--- src/Component/WaitList.js
+++ src/Component/WaitList.js
@@ -5,7 +5,7 @@ import React from "react";
 function WaitList() {
   return (
     <div className="waitlist justify-content-center mt-5">
-      <form className="form-inline launchlist-form" action="https://getlaunchlist.com/s/YWiBgh" method="POST">
+      <form className="form-inline launchlist-form" action="https://getlaunchlist.com/s/nTehE2" method="POST" target="_blank">
         <div className="form-group"><input className="form-control" type="email" name="email" placeholder="Your Email" required/></div>
         <div className="form-group"><button type="submit" className="btn btn-primary">Join the waitlist </button></div>
       </form>

6bfcc33efab021fc6ded610d761224268f6cfe8c
Olamilekan Akindele <olalekan@astract.com.ng>
2022-08-23 00:28:12 +0100

    fixes

--- src/Component/TeamHero.js
+++ src/Component/TeamHero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import heroteam from "../img/teamhero.png";
 import mobile from "../img/teamhero-mobile.png";
+import WaitList from "./WaitList";
 
 function TeamHero() {
   return(
@@ -14,10 +15,11 @@ function TeamHero() {
           <div className="col-md-10 text-center">
             <h2 className='heroteam'>Increase your teams’ productivity</h2>
             {/* <Animation/> */}
-            <div className="d-grid gap-2 d-md-block mt-5">
+            <WaitList/>
+            {/* <div className="d-grid gap-2 d-md-block mt-5">
               <button className="btn btn-primary me-3 py-2 px-5" type="button">Try it now</button>
               <button className="btn btn-outline-dark py-2 px-3 text-secondary" type="button">Send me a brochure</button>
-            </div>
+            </div> */}
           </div>
 				</div>
 			</div>
--- src/Component/WaitList.js
+++ src/Component/WaitList.js
@@ -4,7 +4,7 @@ import React from "react";
 
 function WaitList() {
   return (
-    <div className="waitlist justify-content-center mt-5">
+    <div className="waitlist justify-content-center mt-4">
       <form className="form-inline launchlist-form" action="https://getlaunchlist.com/s/nTehE2" method="POST" target="_blank">
         <div className="form-group"><input className="form-control" type="email" name="email" placeholder="Your Email" required/></div>
         <div className="form-group"><button type="submit" className="btn btn-primary">Join the waitlist </button></div>
